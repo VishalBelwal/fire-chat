5de4b5823e7664dbe2d3183c14a30027de1c58bf
vishalBelwal <vishalbelwal52@gmail.com>
2024-11-22 13:18:48 +0530

    userDetails hardcode for now

--- src/components/userDetails/UserDetails.jsx
+++ src/components/userDetails/UserDetails.jsx
@@ -1,8 +1,93 @@
-import React from 'react'
-import './userDetails.css'
+import React from "react";
+import "./userDetails.css";
 
 export const UserDetails = () => {
   return (
-    <div className='detail'>UserDetails</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="avatar" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg"
+                  alt=""
+                />
+                <span>Photo_name_appear</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg"
+                  alt=""
+                />
+                <span>Photo_name_appear</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg"
+                  alt=""
+                />
+                <span>Photo_name_appear</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg"
+                  alt=""
+                />
+                <span>Photo_name_appear</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="arrow" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button>LogOut</button>
+      </div>
+    </div>
+  );
+};
