0f0247c0df4f66e95d0e768588a8dc2f6827e2c4
vishalBelwal <vishalbelwal52@gmail.com>
2024-12-03 21:36:07 +0530

    firebase initial setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "fire-chat-aafef.firebaseapp.com",
+  projectId: "fire-chat-aafef",
+  storageBucket: "fire-chat-aafef.firebasestorage.app",
+  messagingSenderId: "528931429804",
+  appId: "1:528931429804:web:77da3c4ea5e43c5018ae09",
+  measurementId: "G-MTSHY30GXE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//for user authorization
+export const auth  = getAuth()
+
+//for database connection
+export const db = getFirestore()
+
+//upload anting file
+export const storage = getStorage()
